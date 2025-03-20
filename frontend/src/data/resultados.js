7c337d7f6eb90c29f7a51552da204ad16f6962b7
Kail <phdkail@gmail.com>
2025-03-20 09:17:24 -0500

    Jornada 18

--- frontend/src/data/resultados.js
+++ frontend/src/data/resultados.js
@@ -15,6 +15,24 @@ const resultados = [
     participantes: ["Equipo Amarillo", "Equipo Azul", "Equipo Rojo"],
     imagen: "/images/resultados/16flyer.png"
   }
+
+  {
+    id: 17,
+    evento: "Jornada 17 2025 - Domingo 16/03/2025",
+    lugar: "Arena 7",
+    fecha: "Domingo 16/03/25",
+    participantes: ["Panteras Negras", "Panteras Blancas", "Panteras Rosadas"],
+    imagen: "/images/resultados/17flyer.png"
+  }
+
+  {
+    id: 18,
+    evento: "Jornada 18 2025 - Miércoles 19/03/2025",
+    lugar: "Arena 7",
+    fecha: "Miércoles 19/03/25",
+    participantes: ["Scalogno FC", "Panteras Azules", "Panteras Amarillas"],
+    imagen: "/images/resultados/18flyer.png"
+  }
 ];
 
 export default resultados;
